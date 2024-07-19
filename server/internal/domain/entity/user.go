06515bf4d5f8d208a559684a277c1d383404ad39
Artem Benda <12459262+artem-benda@users.noreply.github.com>
2024-07-19 18:46:50 +0300

    промежуточный коммит

--- server/internal/domain/entity/user.go
+++ server/internal/domain/entity/user.go
@@ -1,1 +1,7 @@
 package entity
+
+type User struct {
+	Id           int64
+	Email        string
+	PasswordHash string
+}
--- server/internal/domain/service/user.go
+++ server/internal/domain/service/user.go
@@ -1,1 +1,7 @@
 package service
+
+type User struct{}
+
+func (u User) Register(username, password string) error {}
+
+func (u User) Login(username, password string) error {}
